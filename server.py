6bfc3160f08e4d6766e84d963b6cabe4f7930cd0
nemesisrc <rajdeepchakraborty.asansol@gmail.com>
2024-12-21 23:32:16 +0530

    portfolio-website-version-0

--- server.py
+++ server.py
@@ -1,5 +1,6 @@
 from flask import Flask, render_template, request, redirect, url_for, jsonify
 
+
 #initialise the flask application
 app = Flask(__name__)
 
@@ -9,26 +10,37 @@ app = Flask(__name__)
 def homepage():
     return render_template('index.html')
 
-#routing the about page
-@app.route('/landing') 
+#routing the experience page
+@app.route('/experience') 
 def landing():
     return render_template('landing.html')
 
-#routing the blog page
-@app.route('/generic') 
+#routing the my-projects page
+@app.route('/projects') 
 def generic():
     return render_template('generic.html')
 
-#routing the blog page
-@app.route('/elements') 
+#routing the education page
+@app.route('/education') 
 def elements():
     return render_template('elements.html')
 
 #routing the blog page
+'''
 @app.route('/home') 
 def home():
     return redirect('/')
+'''
+
 
 #run the flask application
 if __name__=="__main__":
     app.run(debug=True, host='0.0.0.0', port=8080)
+
+
+'''
+i. homepage - about me
+ii. experience - landing
+iii. projects - generic
+iv. education - elements
+'''
